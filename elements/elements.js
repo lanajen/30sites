a1f4fadfcef855f8e8d151507d42eacc99d39ee2
Lana Lee <ljelee@gmail.com>
2014-03-02 15:28:33 -0800

    20th site

---
+++ elements/elements.js
@@ -0,0 +1,293 @@
+var element = new Array(52);
+var symbol = new Array(52);
+var element_num = new Array(52);
+var atm_mass = new Array(52);
+
+element[0] = "Hydrogen";
+symbol[0] = "H";
+element_num[0] = "1";
+atm_mass[0] = "1.01";
+
+element[1] = "Helium";
+symbol[1] = "He";
+element_num[1] = "2";
+atm_mass[1] = "4.00";
+
+element[2] = "Lithium";
+symbol[2] = "Li";
+element_num[2] = "3";
+atm_mass[2] = "6.94";
+
+element[3] = "Beryllium";
+symbol[3] = "Be";
+element_num[3] = "4";
+atm_mass[3] = "9.01";
+
+element[4] = "Boron";
+symbol[4] = "B";
+element_num[4] = "5";
+atm_mass[4] = "10.81";
+
+element[5] = "Carbon";
+symbol[5] = "C";
+element_num[5] = "6";
+atm_mass[5] = "12.01";
+
+element[6] = "Nitrogen";
+symbol[6] = "N";
+element_num[6] = "7";
+atm_mass[6] = "14.01";
+
+element[7] = "Oxygen";
+symbol[7] = "O";
+element_num[7] = "8";
+atm_mass[7] = "16.00";
+
+element[8] = "Fluorine";
+symbol[8] = "F";
+element_num[8] = "9";
+atm_mass[8] = "19.00";
+
+element[9] = "Neon";
+symbol[9] = "N";
+element_num[9] = "10";
+atm_mass[9] = "20.18";
+
+element[10] = "Sodium";
+symbol[10] = "Na";
+element_num[10] = "11";
+atm_mass[10] = "23.00";
+
+element[11] = "Magnesium";
+symbol[11] = "Mg";
+element_num[11] = "12";
+atm_mass[11] = "24.31";
+
+element[12] = "Aluminum";
+symbol[12] = "Al";
+element_num[12] = "13";
+atm_mass[12] = "26.98";
+
+element[13] = "Silicon";
+symbol[13] = "Si";
+element_num[13] = "28.09";
+atm_mass[13] = "28";
+
+element[14] = "Phosporus";
+symbol[14] = "P";
+element_num[14] = "15";
+atm_mass[14] = "30.97";
+
+element[15] = "Sulfur";
+symbol[15] = "S";
+element_num[15] = "16";
+atm_mass[15] = "32.06";
+
+element[16] = "Chlorine";
+symbol[16] = "Cl";
+element_num[16] = "17";
+atm_mass[16] = "35.45";
+
+element[17] = "Argon";
+symbol[17] = "Ar";
+element_num[17] = "18";
+atm_mass[17] = "39.95";
+
+element[18] = "Potassium";
+symbol[18] = "K";
+element_num[18] = "19";
+atm_mass[18] = "39.10";
+
+element[19] = "Calcium";
+symbol[19] = "Ca";
+element_num[19] = "20";
+atm_mass[19] = "40.08";
+
+element[20] = "Scandium";
+symbol[20] = "Sc";
+element_num[20] = "21";
+atm_mass[20] = "44.96";
+
+element[21] = "Titanium";
+symbol[21] = "Ti";
+element_num[21] = "22";
+atm_mass[21] = "47.87";
+
+element[22] = "Vanadium";
+symbol[22] = "V";
+element_num[22] = "23";
+atm_mass[22] = "";
+
+element[23] = "Chromium";
+symbol[23] = "Cr";
+element_num[23] = "24";
+atm_mass[23] = "52.00";
+
+element[24] = "Manganese";
+symbol[24] = "Mn";
+element_num[24] = "25";
+atm_mass[24] = "54.94";
+
+element[25] = "Iron";
+symbol[25] = "Fe";
+element_num[25] = "26";
+atm_mass[25] = "55.85";
+
+element[26] = "Cobalt";
+symbol[26] = "Co";
+element_num[26] = "27";
+atm_mass[26] = "58.93";
+
+element[27] = "Nickel";
+symbol[27] = "Ni";
+element_num[27] = "28";
+atm_mass[27] = "58.69";
+
+element[28] = "Copper";
+symbol[28] = "Cu";
+element_num[28] = "29";
+atm_mass[28] = "63.55";
+
+element[29] = "Zinc";
+symbol[29] = "Zn";
+element_num[29] = "30";
+atm_mass[29] = "65.38";
+
+element[30] = "Gallium";
+symbol[30] = "Ga";
+element_num[30] = "31";
+atm_mass[30] = "69.72";
+
+element[31] = "Germanium";
+symbol[31] = "Ge";
+element_num[31] = "32";
+atm_mass[31] = "72.63";
+
+element[32] = "Arsenic";
+symbol[32] = "As";
+element_num[32] = "33";
+atm_mass[32] = "74.92";
+
+element[33] = "Selenium";
+symbol[33] = "Se";
+element_num[33] = "34";
+atm_mass[33] = "78.96";
+
+element[34] = "Bromine";
+symbol[34] = "Br";
+element_num[34] = "35";
+atm_mass[34] = "79.90";
+
+element[35] = "Krypton";
+symbol[35] = "Kr";
+element_num[35] = "36";
+atm_mass[35] = "83.80";
+
+element[36] = "Rubidium";
+symbol[36] = "Rb";
+element_num[36] = "37";
+atm_mass[36] = "85.47";
+
+element[37] = "Strontium";
+symbol[37] = "Sr";
+element_num[37] = "38";
+atm_mass[37] = "87.62";
+
+element[38] = "Yttrium";
+symbol[38] = "Y";
+element_num[38] = "39";
+atm_mass[38] = "";
+
+element[39] = "Zirconium";
+symbol[39] = "";
+element_num[39] = "40";
+atm_mass[39] = "91.22";
+
+element[40] = "Niobium";
+symbol[40] = "Nb";
+element_num[40] = "41";
+atm_mass[40] = "92.91";
+
+element[41] = "Molybdenum";
+symbol[41] = "Mo";
+element_num[41] = "42";
+atm_mass[41] = "95.96";
+
+element[42] = "Technetium";
+symbol[42] = "Tc";
+element_num[42] = "43";
+atm_mass[42] = "98";
+
+element[43] = "Ruthenium";
+symbol[43] = "Ru";
+element_num[43] = "44";
+atm_mass[43] = "101.07";
+
+element[44] = "Rhodium";
+symbol[44] = "Rh";
+element_num[44] = "45";
+atm_mass[44] = "102.90";
+
+element[45] = "Palladium";
+symbol[45] = "Pd";
+element_num[45] = "46";
+atm_mass[45] = "106.42";
+
+element[46] = "Silver";
+symbol[46] = "Ag";
+element_num[46] = "47";
+atm_mass[46] = "107.87";
+
+element[47] = "Cadmium";
+symbol[47] = "Cd";
+element_num[47] = "48";
+atm_mass[47] = "112.41";
+
+element[48] = "Indium";
+symbol[48] = "In";
+element_num[48] = "49";
+atm_mass[48] = "114.81";
+
+element[49] = "Tin";
+symbol[49] = "Sn";
+element_num[49] = "50";
+atm_mass[49] = "118.71";
+
+element[50] = "Antimony";
+symbol[50] = "Sb";
+element_num[50] = "51";
+atm_mass[50] = "121.60";
+
+element[51] = "Tellurium";
+symbol[51] = "Te";
+element_num[51] = "52";
+atm_mass[51] = "127.60";
+
+element[52] = "Iodine";
+symbol[52] = "I";
+element_num[52] = "53";
+atm_mass[52] = "126.90";
+
+element[53] = "Xenon";
+symbol[53] = "Xe";
+element_num[53] = "54";
+atm_mass[53] = "131.29";
+
+function showInfo() {
+	var page = document.info_form;
+	var choice = page.elementsList;
+
+	for (var i = 0; i <= element.length; i++) {
+		if (choice.options[choice.selectedIndex].value  == element[i]) {
+			page.symbol.value = symbol[i];
+			page.element_num.value = element_num[i];
+			page.atm_mass.value = atm_mass[i];
+			break;
+		}
+		else {
+			page.symbol.value = "";
+			page.element_num.value = "";
+			page.atm_mass.value = "";
+      }
+   }
+}
