3e184fc8f57fa64154677a4cf6c4e4a1e4a8e596
Lana Lee <ljelee@gmail.com>
2014-03-28 19:50:40 -0700

    functioning js

--- christmashappy/christmashappy.js
+++ christmashappy/christmashappy.js
@@ -1,11 +1,16 @@
-var images = ["images/xmas1.jpeg", "images/xmas2.jpg", "images/xmas3.jpeg", "images/xmas4.jpg", "images/xmas5.jpg", "images/xmas6.jpg", "images/xmas7.JPG", "images/xmas8.jpg", "images/xmas9.jpg", "images/xmas10.jpeg"];
+var images = ["images/xmas1.jpeg", "images/xmas2.jpg", "images/xmas3.jpeg", "images/xmas4.jpg", "images/xmas5.jpg", "images/xmas6.jpg", "images/xmas6.jpg", "images/xmas8.jpg", "images/xmas9.jpg", "images/xmas10.jpeg"];
 		
 function changeimage() {
-	var pic = Math.floor(Math.random()*10);
-	document.getElementById("main").style.backgroundImage = url('images[pic]');
+	var index = Math.floor(Math.random()*10);
+    
+    var pic = document.createElement("IMG");
+    pic.style.height = '800px';
+    pic.style.width = '800px';
+    pic.src = images[index];
+    
+    var col = document.getElementById("main");
+    while (col.hasChildNodes()) {
+        col.removeChild(col.lastChild);
+    }
+    col.appendChild(pic);
 }
-//function changeimage() {
-//	var col = document.getElementById("main");
-//	col.style.backgroundImage = "url(images[Math.floor(Math.random()*10)])";
-//}
-			
